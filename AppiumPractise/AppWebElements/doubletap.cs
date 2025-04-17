ad4c1e5d109b4fe6e9f5186c3117054212125ab4
Vaishnavi M <149357291+Vaishnavi53@users.noreply.github.com>
2025-04-17 17:21:37 +0530

    Update doubletap.cs

--- AppiumPractise/AppWebElements/doubletap.cs
+++ AppiumPractise/AppWebElements/doubletap.cs
@@ -1,51 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium.Appium.Android;
-using OpenQA.Selenium.Appium.Enums;
-using OpenQA.Selenium.Appium;
-using OpenQA.Selenium;
+// ﻿using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using OpenQA.Selenium.Appium.Android;
+// using OpenQA.Selenium.Appium.Enums;
+// using OpenQA.Selenium.Appium;
+// using OpenQA.Selenium;
 
-namespace AppiumPractise.AppWebElements
-{
-    public class doubletap
-    {
-        public AndroidDriver driver;
+// namespace AppiumPractise.AppWebElements
+// {
+//     public class doubletap
+//     {
+//         public AndroidDriver driver;
 
-        [OneTimeSetUp]
-        public void setup()
-        {
-           var appPath = Path.Combine(Directory.GetCurrentDirectory(), "ApiDemos-debug (1).apk");
-            var serverUri = new Uri(Environment.GetEnvironmentVariable("APPIUM_HOST") ?? "http://127.0.0.1:4723");
-            var driverOptions = new AppiumOptions()
-            {
-                AutomationName = AutomationName.AndroidUIAutomator2,
-                PlatformName = "Android",
-                DeviceName = "emulator-5554",
+//         [OneTimeSetUp]
+//         public void setup()
+//         {
+//            var appPath = Path.Combine(Directory.GetCurrentDirectory(), "ApiDemos-debug (1).apk");
+//             var serverUri = new Uri(Environment.GetEnvironmentVariable("APPIUM_HOST") ?? "http://127.0.0.1:4723");
+//             var driverOptions = new AppiumOptions()
+//             {
+//                 AutomationName = AutomationName.AndroidUIAutomator2,
+//                 PlatformName = "Android",
+//                 DeviceName = "emulator-5554",
 
-            };
-            driverOptions.AddAdditionalAppiumOption("avd", "medium_phone_api_35");
-            //Initializign appium server 
-            driverOptions.AddAdditionalAppiumOption("Application", appPath);
-            driverOptions.AddAdditionalAppiumOption("noReset", "true");
-            driver = new AndroidDriver(serverUri, driverOptions, TimeSpan.FromSeconds(180));
+//             };
+//             driverOptions.AddAdditionalAppiumOption("avd", "medium_phone_api_35");
+//             //Initializign appium server 
+//             driverOptions.AddAdditionalAppiumOption("Application", appPath);
+//             driverOptions.AddAdditionalAppiumOption("noReset", "true");
+//             driver = new AndroidDriver(serverUri, driverOptions, TimeSpan.FromSeconds(180));
 
-        }
-        [OneTimeTearDown]
-        public void TearDown()
-        {
-            driver.Dispose();
-            driver.Quit();
-        }
-        [Test]
-        public void doubletaptest()
-        {
-            IWebElement view = driver.FindElement(By.XPath("//android.widget.TextView[@content-desc=\"Views\"]"));
-            view.Click();
-        }
+//         }
+//         [OneTimeTearDown]
+//         public void TearDown()
+//         {
+//             driver.Dispose();
+//             driver.Quit();
+//         }
+//         [Test]
+//         public void doubletaptest()
+//         {
+//             IWebElement view = driver.FindElement(By.XPath("//android.widget.TextView[@content-desc=\"Views\"]"));
+//             view.Click();
+//         }
 
 
-    }
-}
+//     }
+// }
